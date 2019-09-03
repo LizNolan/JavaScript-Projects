8e67397b8d0e92b85399aa31283af627864ddac0
LizNolan <liznolanpdx@gmail.com>
2019-09-03 13:42:40 -0700

    Basic Javascript Project

---
+++ Basic JavaScript Projects - Copy/JS/Basic_JavaScript_1.js
@@ -0,0 +1,16 @@
+
+
+var Pets="animals", dog="Fido", cat="Tabitha", bird="Squeaky";
+var Pets= Pets.fontcolor("yellow");
+document.write(Pets);
+
+var dog= dog.fontcolor("red");
+document.write (dog);
+
+var cat=cat.fontcolor ("pink");
+document.write (cat);
+
+var bird= bird.fontcolor("orange");
+document.write (bird);
+
+document.write(4+5);
