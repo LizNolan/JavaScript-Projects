a6ba8497fddf66e941838acbf300917b3f7a90fd
LizNolan <liznolanpdx@gmail.com>
2019-09-04 12:17:59 -0700

    Second JavaScript project

---
+++ Basic JavaScript Projects - Copy/JS/Basic_JavaScript_2.js
@@ -0,0 +1,10 @@
+function My_First_Function () {
+    var String="This text is red!";
+    var result=String.fontcolor("red");
+    document.getElementById("Red_Text").innerHTML=result;
+}
+function myFunction() {
+    var sentence="I am learning";
+    sentence +=" a lot from this book!";
+    document.getElementById("concantenate").innerHTML=sentence;
+}
